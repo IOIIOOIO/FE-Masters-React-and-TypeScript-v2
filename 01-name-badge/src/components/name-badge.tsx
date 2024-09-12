6e90f56def321d68889a482ee3324fa5e33553fa
James Wilson <jamesjw01@gmail.com>
2024-09-12 18:32:24 +0200

    exercise(name-badge): Typing Components

--- 01-name-badge/src/components/name-badge.tsx
+++ 01-name-badge/src/components/name-badge.tsx
@@ -1,12 +1,18 @@
-const NameBadge = () => {
+type NameBadgePropTypes = {
+    greeting?: string | 'Hi';
+    name?: string | 'No Name';
+}
+
+
+const NameBadge = ({greeting, name}: NameBadgePropTypes) => {
   return (
     <section className="badge">
       <header className="badge-header">
-        <h1 className="text-5xl">HELLO</h1>
+        <h1 className="text-5xl">{greeting}</h1>
         <p>My name is…</p>
       </header>
       <div className="badge-body">
-        <p className="badge-name">Steve</p>
+        <p className="badge-name">{name}</p>
       </div>
       <footer className="badge-footer" />
     </section>
